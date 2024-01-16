53f5dbdff4f774d9e7c3669d9e3a5713d8898d5d
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-01-16 11:05:07 +0100

    Revert "npm dependency uninstall Axios, PWA"
    This reverts commit b95ebbf4f45495f5b31fbaba5d1ba57d4e18fd0f.

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,25 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/'
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
