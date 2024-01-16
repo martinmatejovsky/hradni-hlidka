3d9ba6df2aa8fc601eac5bc18c378f0ee4ac9caa
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-01-16 20:44:51 +0100

    Vuetify treeshaking

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar title="Hradní hlídka"></v-app-bar>
-    <v-main>
-      <slot></slot>
-    </v-main>
+    <v-container class="bg-surface-variant mb-6">
+      <v-app-bar title="Hradní hlídka"></v-app-bar>
+      <v-main>
+        <slot></slot>
+      </v-main>
+    </v-container>
   </v-app>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,11 @@ export default defineNuxtConfig({
     transpile: ['vuetify'],
   },
   modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
+          vuetify()
+      ))
+    },
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
