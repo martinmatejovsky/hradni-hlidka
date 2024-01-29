32fe5195550ce85f9f31b640ab4159d8688769ad
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-01-29 23:45:52 +0100

    feat(ladder): new component Ladder

---
+++ components/ClimbingLadder.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type {Invader} from "~/types/CustomTypes";
+
+defineProps({
+    climbingInvaders: {
+      type: Array as PropType<(Invader | null)[]>,
+      required: true
+    }
+});
+</script>
+
+<template>
+<div class="climbing-ladder">
+  <div v-for="(invader, index) in climbingInvaders" :key="index" class="climbing-ladder-step">
+    <v-icon v-if="invader !== null" icon="mdi-sword" class="climbing-ladder-invader-icon"></v-icon>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+.climbing-ladder {
+  display: flex;
+  flex-basis: 100%;
+}
+
+.climbing-ladder-step {
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 40px;
+  border: 2px solid #E6E6E6;
+}
+.climbing-ladder-invader-icon {
+  position: absolute;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
 
     <v-btn v-if="storedGameState === 'ready'" @click="startAttack" rounded="xs" class="mt-3 mb-3">Zahájit útok</v-btn>
     <div v-else-if="storedGameState === 'lost'">
-      <h4 class="text-h4 mb-4 text-red">Prohráli jste.</h4>
+      <h4 class="text-h4 mb-4 text-red">Prohráli jste</h4>
       <v-btn @click="restartAttack" rounded="xs" class="mb-6">Znovu na ně!</v-btn>
     </div>
 
@@ -23,18 +23,11 @@
         <div v-for="attackedArea in storedAreaAttackStat" class="mb-3" :key="attackedArea.areaName">
           <h4 class="text-amber">{{ attackedArea.areaName }}</h4>
           <p>strážce: {{ attackedArea.guardians[0]?.name || '--' }}</p>
-          <p>Shromážděni: {{ attackedArea.assembledInvaders.length }}</p>
-          <p>Žebřik: {{ attackedArea.assaultLadder }}</p>
-          <v-progress-linear
-              v-model="attackedArea.threatLevel"
-              bg-color="lime-darken-4"
-              :color="attackedArea.conquered ? 'error' : 'orange-darken-4'"
-              height="25"
-          >
-            <template v-slot:default="{ value }">
-              <strong>{{ Math.ceil(value) }} %</strong>
-            </template>
-          </v-progress-linear>
+          <p>Shromáždění útočníci:
+            <v-icon v-for="(invader, index) in attackedArea.assembledInvaders" :key="index" icon="mdi-sword"></v-icon>
+          </p>
+          <p>Žebřik <v-icon icon="mdi-arrow-right-bold-outline"></v-icon></p>
+          <ClimbingLadder :climbingInvaders="attackedArea.assaultLadder" />
         </div>
       </div>
     </div>
