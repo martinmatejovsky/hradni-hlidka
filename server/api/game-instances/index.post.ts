a67354b4d2dd05e3caa4c0eaca32d9974360435b
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-02-04 21:30:45 +0100

    feat(API): saving new game instance into .json file

--- server/api/game-instances/index.post.ts
+++ server/api/game-instances/index.post.ts
@@ -1,4 +1,5 @@
 import {GameInstance, PlayerData, BattleZone} from "~/types/CustomTypes";
+import { writeFile } from 'fs/promises';
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
@@ -16,11 +17,24 @@ export default defineEventHandler(async (event) => {
         players: new Array(body.hostingPlayer as PlayerData),
     }
 
-    return {
-        status: 201, // 201 Created
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: newGameInstance,
-    };
+    const dataDirectory = 'server/game-instances/';
+    const instanceFileName = `${newGameInstance.id}.json`;
+    const instanceFilePath = `${dataDirectory}${instanceFileName}`;
+
+    try {
+        await writeFile(instanceFilePath, JSON.stringify(newGameInstance, null, 2), 'utf-8');
+
+        return {
+            status: 201, // 201 Created
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: newGameInstance,
+        };
+    } catch (error) {
+        return {
+            status: 500, // 500 Internal Server Error
+            body: { error: 'Error writing game instance to server file ' + instanceFilePath },
+        };
+    }
 });
