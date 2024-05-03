33d05b392859f1aa273b111a7004249f436953ed
Martin Matejovsky <martin.matejovsky@jpower8.cz>
2024-05-03 17:59:47 +0200

    feat(map): installed Leaflet as a Node module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
         config.plugins.push(vuetify({ autoImport: true }))
       })
     },
+    'nuxt3-leaflet',
   ],
   app: {
     head: {
--- pages/map.vue
+++ pages/map.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const zoom = ref(20)
 </script>
 
 <template>
 <h1>Mapa</h1>
 
+<div style="height:70vh; width:70vw">
+  <LMap
+      ref="map"
+      :zoom="zoom"
+      :center="[50.1912094, 12.7429419]"
+  >
+    <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+        layer-type="base"
+        name="OpenStreetMap"
+    />
+  </LMap>
+</div>
 </template>
 
 <style scoped>
