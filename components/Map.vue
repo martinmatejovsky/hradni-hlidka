fe95decbb4689c2872ca9b37dc8dbda0b498217e
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-05-29 07:29:57 +0200

    feat(map): component Map.vue from imported javascript library instead of Nuxt component

--- components/Map.vue
+++ components/Map.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+useHead({
+  script: [ { src: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', defer: true, integrity: "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=", crossorigin: "" } ],
+  link: [{ href: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', integrity: "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=", crossorigin: "" }]
+})
+
 import {useState} from "nuxt/app";
-import type {Invader, PlayerData} from "~/types/CustomTypes";
+import type {PlayerData} from "~/types/CustomTypes";
 import {STORE_CURRENT_PLAYER} from "~/constants";
 import horseRiderIcon from '~/assets/icons/horse-rider.svg';
 
 const currentPlayer = useState<PlayerData>(STORE_CURRENT_PLAYER);
-const otherPlayers = ref<PlayerData[]>([]);
 const zoom = ref(11)
 
 defineProps({
@@ -14,41 +18,74 @@ defineProps({
     required: true
   },
 });
+
+onMounted(() => {
+  let map = L.map('map').setView([50.1912094, 12.7429419], zoom.value);
+
+  let currentPlayerIcon = L.divIcon({
+    className: 'h-rider-icon',
+    html: `<img class="h-rider-icon-image" alt="" src="${horseRiderIcon}"/>`+
+        '<span class="h-rider-icon-description">{{ currentPlayer.name }}</span>',
+  });
+
+  L.TileLayer.Battlefield = L.TileLayer.extend({
+    getTileUrl: function(coords) {
+      return `/map-layers/{z}/{x}/{y}.png`
+          .replace('{z}', coords.z)
+          .replace('{x}', coords.x)
+          .replace('{y}', coords.y);
+    },
+    getAttribution: function() {
+      return '&copy; Martin Matějovský, bejby'
+    }
+  });
+
+  L.tileLayer.battlefield = function() {
+    return new L.TileLayer.Battlefield();
+  }
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  L.tileLayer.battlefield().addTo(map);
+
+  // TODO: this does not work. How to get it from defineProps?
+  connectedPlayers.value.forEach((player: PlayerData) => {
+    if (player.key !== currentPlayer.value.key) {
+      let otherPlayerIcon = L.divIcon({
+        className: 'h-rider-icon',
+        html: `<img class="h-rider-icon-image" alt="" src="${horseRiderIcon}"/>`+
+            '<span class="h-rider-icon-description">{{ player.name }}</span>',
+      });
+      L.marker([player.location.latitude, player.location.longitude], {icon: otherPlayerIcon}).addTo(map)
+    }
+  })
+
+  L.marker([50.1912094, 12.7429419], {icon: currentPlayerIcon}).addTo(map)
+
+})
 </script>
 
 <template>
   <p>connected players count: {{connectedPlayers?.length}}</p>
 
-  <div style="height:70vh; width:100%">
-    <LMap
-        ref="map"
-        :zoom="zoom"
-        :center="[50.1912094, 12.7429419]"
-    >
-      <LTileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-          layer-type="base"
-          name="OpenStreetMap"
-      />
-      <LTileLayer
-          url="/map-layers/{z}/{x}/{y}.png"
-          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> + copy; Martin Matějovský bejby contributors"
-          layer-type="base"
-          name="OpenStreetMap"
-      />
-
-      <template v-for="(player) in connectedPlayers" :key="player.key">
-        <l-marker :lat-lng="[player.location.latitude, player.location.longitude]">
-          {{ player.name }}
-          <l-icon :iconUrl="horseRiderIcon" :icon-size="[25, 25]" alt="player.name" title="player.name"/>
-        </l-marker>
-      </template>
-    </LMap>
-  </div>
+  <div id="map"></div>
 </template>
 
 <style>
+#map {
+  height: 60vh;
+  width: 100%;
+}
+
+.h-rider-icon-description {
+  color: #000000;
+  font-size: 14px;
+  font-weight: bold;
+}
+
 .leaflet-marker-icon {
   transition: all 0.3s;
 }
